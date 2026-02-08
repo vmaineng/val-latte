39bec931db509104d91c975985ef62e4c3e23fce
Mai Vang <vmaineng@gmail.com>
2026-02-08 12:58:01 -0800

    added in wheelsegment

---
+++ app/components/Wheelsegment.tsx
@@ -0,0 +1,42 @@
+import { WheelOption } from "../types";
+import { calculateSegmentAngles, getClipPath } from "../utils/Wheelhelpers";
+
+interface WheelSegementProps {
+  option: WheelOption;
+  index: number;
+  totalSegements: number;
+}
+
+export const WheelSegment: React.FC<WheelSegementProps> = ({
+  option,
+  index,
+  totalSegments,
+}) => {
+  const { angle, nextAngle } = calculateSegmentAngles(index, totalSegments);
+  const clipPath = getClipPath(angle, nextAngle);
+  const labelRotation = angle + 360 / totalSegments / 2;
+
+  return (
+    <div
+      className={`absolute w-full h-full ${option.color}`}
+      style={{ clipPath }}
+    >
+      <div
+        className="absolute w-full h-full flex items-center justify-center text-center px-4"
+        style={{
+          transform: `rotate(${labelRotation}deg)`,
+          transformOrigin: "center",
+        }}
+      >
+        <span
+          className="font-semibold text-lg block text-amber-900 whitespace-nowrap"
+          style={{
+            transform: "translateY(-120px) rotate(-90deg)",
+          }}
+        >
+          {option.text}
+        </span>
+      </div>
+    </div>
+  );
+};
